02da7202553b5bd66e8239e401366b855f22383b
Praful Kale <prafulkale123@gmail.com>
2018-06-20 15:13:54 +0530

    merged comment

--- app/src/main/java/com/example/prafulkale/githubpoc/MainActivity.java
+++ app/src/main/java/com/example/prafulkale/githubpoc/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 // i am in version2.0 branch
+    // merged
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
