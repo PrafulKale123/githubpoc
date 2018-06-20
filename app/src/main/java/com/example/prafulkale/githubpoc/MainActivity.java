3c79a08957120841e1ec31985afad1becd7bd80f
Praful Kale <prafulkale123@gmail.com>
2018-06-20 13:42:57 +0530

    comment added

--- app/src/main/java/com/example/prafulkale/githubpoc/MainActivity.java
+++ app/src/main/java/com/example/prafulkale/githubpoc/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// i am in version2.0 branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
